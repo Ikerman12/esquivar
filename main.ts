8e3fb5eb1c9e40ac96783f60abd26c3882be6243
Ikerman12 <122780634+Ikerman12@users.noreply.github.com>
2023-01-16 10:28:32 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,19 @@
 input.onButtonPressed(Button.A, function () {
-    JUGADOR_1.change(LedSpriteProperty.X, -1)
+    JUGADOR_ARRIBA.change(LedSpriteProperty.Y, 1)
+    JUGADOR_ABAJO.change(LedSpriteProperty.Y, 1)
+    basic.pause(100)
+    JUGADOR_ARRIBA.change(LedSpriteProperty.Y, 1)
+    JUGADOR_ABAJO.change(LedSpriteProperty.Y, 1)
 })
 input.onButtonPressed(Button.B, function () {
-    JUGADOR_1.change(LedSpriteProperty.X, 1)
-})
-let OBJETO: game.LedSprite = null
-let JUGADOR_1: game.LedSprite = null
-JUGADOR_1 = game.createSprite(2, 4)
-basic.forever(function () {
-    basic.pause(randint(1000, 1500))
-    OBJETO = game.createSprite(randint(0, 4), 2)
-    for (let index = 0; index < 4; index++) {
-    	
-    }
+    JUGADOR_ARRIBA.change(LedSpriteProperty.X, -1)
+    JUGADOR_ABAJO.change(LedSpriteProperty.Y, 1)
     basic.pause(100)
-    JUGADOR_1.change(LedSpriteProperty.Y, 1)
-    if (JUGADOR_1.isTouching(OBJETO)) {
-        game.gameOver()
-    }
-    basic.pause(100)
-    game.addScore(1)
-    OBJETO.delete()
+    JUGADOR_ARRIBA.change(LedSpriteProperty.X, 1)
+    JUGADOR_ABAJO.change(LedSpriteProperty.Y, -1)
 })
+let JUGADOR_ABAJO: game.LedSprite = null
+let JUGADOR_ARRIBA: game.LedSprite = null
+JUGADOR_ARRIBA = game.createSprite(1, 3)
+JUGADOR_ABAJO = game.createSprite(1, 4)
+let OBJETO = game.createSprite(4, 4)
